a4b1f2b7a05ae01d7598745cb865caddee5524f1
TomTom <93038247+AverageGithub@users.noreply.github.com>
2023-09-21 17:56:55 +0200

    finishing touches

--- src/main/java/hu/rivalsnetwork/rivalsbackups/compress/GZIPOutputStream.java
+++ src/main/java/hu/rivalsnetwork/rivalsbackups/compress/GZIPOutputStream.java
@@ -2,12 +2,10 @@ package hu.rivalsnetwork.rivalsbackups.compress;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.zip.Deflater;
 
 public class GZIPOutputStream extends java.util.zip.GZIPOutputStream {
 
     public GZIPOutputStream(OutputStream out) throws IOException {
         super(out);
-        def.setLevel(Deflater.BEST_COMPRESSION);
     }
 }
