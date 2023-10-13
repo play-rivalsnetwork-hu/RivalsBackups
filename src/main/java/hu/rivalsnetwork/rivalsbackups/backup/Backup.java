e193ec1ac7b993c457369b31d20670f57e07590c
TomTom <93038247+AverageGithub@users.noreply.github.com>
2023-10-13 19:24:14 +0200

    This still needs some fixes

--- src/main/java/hu/rivalsnetwork/rivalsbackups/backup/Backup.java
+++ src/main/java/hu/rivalsnetwork/rivalsbackups/backup/Backup.java
@@ -3,6 +3,7 @@ package hu.rivalsnetwork.rivalsbackups.backup;
 import hu.rivalsnetwork.rivalsbackups.Main;
 import hu.rivalsnetwork.rivalsbackups.compress.Compressor;
 import hu.rivalsnetwork.rivalsbackups.config.Config;
+import hu.rivalsnetwork.rivalsbackups.scheduler.BackupTimer;
 import hu.rivalsnetwork.rivalsbackups.sftp.Uploader;
 import hu.rivalsnetwork.rivalsbackups.utils.StringUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -29,14 +30,22 @@ public class Backup {
     private File compressedFile = null;
     private long startingSize;
     private long finishSize = 0;
-    private final LocalDateTime startDate;
+    private LocalDateTime startDate;
     private Uploader uploader;
     private MessageCreateAction action;
     private long compressedSize = 0;
 
-    public Backup(File file, LocalDateTime startDate) {
+    public Backup(File file) {
         this.stage = BackupStage.STARTING;
         this.file = file;
+    }
+
+    public boolean isSpaceEnough() {
+        // We should assume a 10% compression, so there are no oversights
+        return startingSize * 0.90 < (double) new File("/").getFreeSpace();
+    }
+
+    public void start(LocalDateTime startDate) {
         this.startDate = startDate;
 
         startingSize = FileUtils.sizeOfDirectory(file);
@@ -46,15 +55,7 @@ public class Backup {
 
         Main.getRunningBackups().add(this);
         updateEmbed();
-        start();
-    }
 
-    public boolean isSpaceEnough() {
-        // We should assume a 10% compression, so there are no oversights
-        return startingSize * 0.90 < (double) new File("/").getFreeSpace();
-    }
-
-    public void start() {
         Compressor compressor = new Compressor(this);
         try {
             compressingStart = System.currentTimeMillis();
@@ -70,12 +71,21 @@ public class Backup {
         updateEmbed();
 
         stage = BackupStage.UPLOADING;
+        System.out.println("Countdown!");
+        BackupTimer.countdown.countDown();
         // Start uploading!
         uploader = new Uploader();
         try {
             uploader.setup(this);
             uploadStart = System.currentTimeMillis();
-            uploader.upload(file.getName(), compressedFile);
+            new Thread(() -> {
+                try {
+                    System.out.printf("Uploading: %s, %s", compressedFile, file.getName());
+                    uploader.upload(file.getName(), compressedFile);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -91,8 +101,8 @@ public class Backup {
 
             TextChannel channel = Main.getJda().getGuildById(Config.GUILD_ID).getTextChannelById(Config.CHANNEL_ID);
 
-             action = channel.sendMessageEmbeds(embed.build());
-             message = action.complete();
+            action = channel.sendMessageEmbeds(embed.build());
+            message = action.complete();
         }
 
         if (stage == BackupStage.COMPRESSING || stage == BackupStage.UPLOADING) {
@@ -102,7 +112,7 @@ public class Backup {
             embed.addField(new MessageEmbed.Field(Config.BACKUP_RUNNING_COMPRESSING_TITLE, Config.BACKUP_RUNNING_COMPRESSING_CONTENT.replace("$progress", compressingProgress + "%").replace("$bar", StringUtils.createProgressBar(Config.BACKUP_RUNNING_COMPRESSING_PROGRESSBAR_NOT_DONE, Config.BACKUP_RUNNING_COMPRESSING_PROGRESSBAR_DONE, Config.BACKUP_RUNNING_COMPRESSING_PROGRESSBAR_AMOUNT, compressingProgress)).replace("$elapsed", StringUtils.fancyTime(System.currentTimeMillis() - compressingStart)).replace("$startingSize", String.format("%.2fGB", convertBytesToGigabytes(startingSize))).replace("$finishedSize", String.format("%.2fGB", convertBytesToGigabytes(finishSize))).replace("$percent", compressingProgress + "%"), false));
             embed.addField(new MessageEmbed.Field(Config.BACKUP_RUNNING_UPLOADING_TITLE, Config.BACKUP_RUNNING_UPLOADING_CONTENT.replace("$progress", uploadProgress + "%").replace("$bar", StringUtils.createProgressBar(Config.BACKUP_RUNNING_UPLOADING_PROGRESSBAR_NOT_DONE, Config.BACKUP_RUNNING_UPLOADING_PROGRESSBAR_DONE, Config.BACKUP_RUNNING_UPLOADING_PROGRESSBAR_AMOUNT, uploadProgress)).replace("$elapsed", uploadStart != 0 ? StringUtils.fancyTime(System.currentTimeMillis() - uploadStart) : "00:00:00").replace("$percent", uploadProgress + "%"), false));
 
-            MessageEditAction action1 =  message.editMessageEmbeds(embed.build());
+            MessageEditAction action1 = message.editMessageEmbeds(embed.build());
             action1.setEmbeds(embed.build()).complete();
         }
     }
--- src/main/java/hu/rivalsnetwork/rivalsbackups/scheduler/BackupTimer.java
+++ src/main/java/hu/rivalsnetwork/rivalsbackups/scheduler/BackupTimer.java
@@ -4,15 +4,21 @@ import hu.rivalsnetwork.rivalsbackups.Main;
 import hu.rivalsnetwork.rivalsbackups.backup.Backup;
 import hu.rivalsnetwork.rivalsbackups.config.Config;
 import hu.rivalsnetwork.rivalsbackups.sftp.Uploader;
+import org.apache.commons.collections4.QueueUtils;
 
 import java.io.File;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 
 public class BackupTimer implements Runnable {
     private int i = 0;
     private LocalDateTime lastRan = LocalDateTime.now();
+    private final Queue<Backup> backupQueue = new ArrayDeque<>();
+    public static final CountDownLatch countdown = new CountDownLatch(1);
 
     @Override
     public void run() {
@@ -35,11 +41,17 @@ public class BackupTimer implements Runnable {
             File serverFolder = new File(Config.SERVER_DIRECTORY);
             for (File file : serverFolder.listFiles()) {
                 if (!file.isDirectory()) continue;
+                if (file.getName().contains(".sftp")) continue;
 
                 for (String s : Config.DIFFERENT_SAVE_INTERVAL) {
                     if (file.getName().contains(s)) {
+                        backupQueue.add(new Backup(file));
+
                         Executors.newSingleThreadScheduledExecutor().execute(() -> {
-                            new Backup(file, now);
+                            Backup backup;
+                            while ((backup = backupQueue.peek()) != null) {
+                                backup.start(now);
+                            }
                         });
                         break;
                     }
@@ -63,14 +75,28 @@ public class BackupTimer implements Runnable {
             File serverFolder = new File(Config.SERVER_DIRECTORY);
             x: for (File file : serverFolder.listFiles()) {
                 if (!file.isDirectory()) continue;
+                if (file.getName().contains(".sftp")) continue;
+
                 for (String s : Config.DIFFERENT_SAVE_INTERVAL) {
                     if (file.getName().contains(s)) continue x;
                 }
 
-                Executors.newSingleThreadScheduledExecutor().execute(() -> {
-                    new Backup(file, now);
-                });
+                backupQueue.add(new Backup(file));
             }
+
+            Executors.newSingleThreadScheduledExecutor().execute(() -> {
+                Backup backup;
+                countdown.countDown();
+                while ((backup = backupQueue.poll()) != null) {
+                    try {
+                        countdown.await();
+
+                        backup.start(now);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
         }
 
         i++;
